193e1322c516fd37d2794a1bfb58a6e20847b746
JPLemelin <JPLemelin@Gmail.com>
2017-01-31 22:08:16 -0500

    mdl design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,27 @@ export class AppComponent {
   devices_history: FirebaseListObservable<any[]>;
   settings: FirebaseListObservable<any[]>;
   
+  user = null;
   constructor(public af: AngularFire) {
     this.devices_current_status = af.database.list('/current');
     this.devices_history = af.database.list('/history');
     this.settings = af.database.list('/settings');
+
+    this.af.auth.subscribe(user => {
+      if(user) {
+        // user logged in
+        this.user = user.auth;
+        this.user = {
+          displayName: user.auth.displayName,
+          email: user.auth.email,
+          photoURL: user.auth.photoURL +'?sz=32',
+        };
+      }
+      else {
+        // user not logged in
+        this.user = null;
+      }
+    });
   }
   
   login() {
